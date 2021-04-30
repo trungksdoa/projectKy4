f9e84a761de69814e45efbfbd91d2f23f5697518
trungksdoa <55863018+trungksdoa@users.noreply.github.com>
2021-04-30 23:59:42 +0700

    sdasda
    sdac

---
+++ src/main/java/com/example/demo/HomeRestController.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ *
+ * @author trung
+ */
+@RestController
+@RequestMapping("/url")
+public class HomeRestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Connect Sucess - Ha Trung Try to connect database using this !!!";
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "Error message")
+    public void handleError() {
+    }
+
+}
